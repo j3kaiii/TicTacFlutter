88376f74c5c5a550e8381f6026ce8c916d16d738
j3kaiii <j3kaiii@gmail.com>
2021-08-29 20:57:57 +0300

    added functions checkWinner and showWinDialog

--- lib/main.dart
+++ lib/main.dart
@@ -21,7 +21,19 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  String sign = 'X'; // значек на поле
+  bool onTurn = true; // Чей ход? У нас обычно начинают с X
+
+  List<String> sign = [
+    '',
+    '',
+    '',
+    '',
+    '',
+    '',
+    '',
+    '',
+    '',
+  ]; // значек на поле
 
   @override
   Widget build(BuildContext context) {
@@ -35,7 +47,9 @@ class _HomePageState extends State<HomePage> {
           itemBuilder: (BuildContext context, int index) {
             return GestureDetector(
               // ловим тап
-              onTap: _tapped, // событие при тапе
+              onTap: () {
+                _tapped(index); // в функцию попадает индекс ячейки
+              }, // событие при тапе
               child: Container(
                 // рисуем плитки поля
                 decoration: BoxDecoration(
@@ -44,7 +58,7 @@ class _HomePageState extends State<HomePage> {
                 child: Center(
                   // с текстом по центру
                   child: Text(
-                    sign,
+                    sign[index],
                     style: TextStyle(color: Colors.white, fontSize: 40),
                   ),
                 ),
@@ -54,10 +68,65 @@ class _HomePageState extends State<HomePage> {
     );
   }
 
-  void _tapped() {
+  void _tapped(int index) {
     // стейт можно изменить только внутри setState
     setState(() {
-      sign = 'O';
+      if (onTurn) {
+        sign[index] = 'X';
+      } else {
+        sign[index] = 'O';
+      }
+
+      onTurn = !onTurn; // переключатель хода между Х и О
+      _checkWinner();
     });
+  }
+
+  void _checkWinner() {
+    // проверяем первую строку
+    // все три ячейки одинаковые и при этом не пустые
+    if (sign[0] == sign[1] && sign[0] == sign[2] && sign[0] != '') {
+      _showWinDialog(sign[0]);
+    }
+    // вторая строка
+    if (sign[3] == sign[4] && sign[3] == sign[5] && sign[3] != '') {
+      _showWinDialog(sign[3]);
+    }
+    // тертья строка
+    if (sign[6] == sign[7] && sign[6] == sign[8] && sign[6] != '') {
+      _showWinDialog(sign[6]);
+    }
+
+    // правый столбец
+    if (sign[0] == sign[3] && sign[0] == sign[6] && sign[0] != '') {
+      _showWinDialog(sign[0]);
+    }
+    // средний столбец
+    if (sign[1] == sign[4] && sign[1] == sign[7] && sign[1] != '') {
+      _showWinDialog(sign[1]);
+    }
+    // левый столбец
+    if (sign[2] == sign[5] && sign[2] == sign[8] && sign[2] != '') {
+      _showWinDialog(sign[2]);
+    }
+
+    // одна диагональ
+    if (sign[0] == sign[4] && sign[0] == sign[8] && sign[0] != '') {
+      _showWinDialog(sign[0]);
+    }
+    // другая диагональ
+    if (sign[2] == sign[4] && sign[2] == sign[6] && sign[2] != '') {
+      _showWinDialog(sign[2]);
+    }
+  }
+
+  void _showWinDialog(String winner) {
+    showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: Text('WINNER IS $winner'),
+          );
+        });
   }
 }
